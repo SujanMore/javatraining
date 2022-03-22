6d55bb7127fb2d1e792df83605cce7d5ab9b6aa8
SujanMore <sujanmore@gmail.com>
2022-03-22 17:26:34 +0530

    Multiply two numbers

---
+++ Day1/Multiply.java
@@ -0,0 +1,10 @@
+public class Multiply {
+    public static void main(String[] args) {
+
+        int i = 0;
+
+        i = 5 * 3;  //Multiply two numbers
+
+        System.out.println(i);
+    }
+}
