8523cacea7fb7dc3b927cd083f3d945e89839d7a
SujanMore <sujanmore@gmail.com>
2022-03-22 18:12:50 +0530

    Find Greater combination of variable

---
+++ Day1/GreaterCheck.java
@@ -0,0 +1,11 @@
+public class GreaterCheck {
+    public static void main(String[] args) {
+        int a = 5, b = 9, c = 7, d = 3;
+
+        if(a + b > c + d){                                      //If condition to fnd greater combination
+            System.out.println("a + b is greater then c + d");
+        } else {
+            System.out.println("c + d is greater then a + b");
+        }
+    }
+}
