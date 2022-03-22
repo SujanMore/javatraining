9f5bea73c4af6be86d66a404c06e7f31d8844611
SujanMore <sujanmore@gmail.com>
2022-03-22 18:38:36 +0530

    Print the Multiplication Table of the entered number.

---
+++ Day1/MultiplicationTable.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int num = input.nextInt();
+
+        for(int i = 1; i <= 10; i++){
+            System.out.println(num + " * " + i + " = " + num * i);
+        }
+    }
+}
