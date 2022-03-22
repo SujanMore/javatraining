76f49a98835318bdafae5a00900aaf830d3bffde
SujanMore <sujanmore@gmail.com>
2022-03-22 19:08:45 +0530

    Print Hello World Thrice using method.

---
+++ Day2/SayHelloWorldThrice.java
@@ -0,0 +1,10 @@
+public class SayHelloWorldThrice {
+
+    static void printHelloThrice(){
+        for(int i = 0; i < 3; i++)
+        System.out.println("Hello World");
+    }
+    public static void main(String[] args) {
+        printHelloThrice();             //Call the method
+    }
+}
