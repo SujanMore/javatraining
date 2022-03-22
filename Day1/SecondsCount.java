e3d8103e8739ad49c6e6970bb3da44e9497ef502
SujanMore <sujanmore@gmail.com>
2022-03-22 16:48:20 +0530

    Count number of seconds in a day.

---
+++ Day1/SecondsCount.java
@@ -0,0 +1,9 @@
+public class SecondsCount {
+    public static void main(String[] args) {
+
+        int totalSeconds = 0;
+
+        totalSeconds = 24 * 60 * 60;     //Calculate total minutes in 24 hour
+        System.out.println(totalSeconds);
+    }
+}
