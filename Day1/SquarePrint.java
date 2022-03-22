35393accee56fb7ab6780562d2d71859605b4feb
SujanMore <sujanmore@gmail.com>
2022-03-22 18:59:58 +0530

    Print square of 10 numbers, even & odd.

--- Day1/SquarePrint.java
+++ Day1/SquarePrint.java
@@ -7,12 +7,14 @@ public class SquarePrint {
             System.out.println(i + " * " + i + " = " + i * i);
         }
 
+        System.out.println("");
         System.out.println("Square of even numbers: ");
         for(int i = 1; i <= 10; i++) {                            //Print Square of 10 Even Numbers
             if(i % 2 == 0)
                 System.out.println(i + " * " + i + " = " + i * i);
         }
 
+        System.out.println("");
         System.out.println("Square of odd numbers: ");
         for(int i = 1; i <= 10; i++) {                            //Print Square of 10 Odd Numbers
             if(i % 2 != 0)
