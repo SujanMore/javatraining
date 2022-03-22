7f794d1bb73cfcc26817990201b1567ac8ba8bc6
SujanMore <sujanmore@gmail.com>
2022-03-22 16:36:59 +0530

    Exercise for calculatng minutes in a day.

---
+++ Day1/MinutesCount.java
@@ -0,0 +1,10 @@
+public class MinutesCount {
+    public static void main(String[] args) {
+
+        int totalMinutes = 0;
+
+        totalMinutes = 24 * 60;     //Calculate total minutes in 24 hours
+
+        System.out.println(totalMinutes);
+    }
+}
