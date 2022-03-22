4c24669ccabd633304fbb081577bac1e968b4d9a
SujanMore <sujanmore@gmail.com>
2022-03-22 17:45:31 +0530

    Sum of three variables

--- Day1/Sum.java
+++ Day1/Sum.java
@@ -3,13 +3,11 @@ public class Sum {
         int a = 5, b = 6, c = 7;
 
         int result = a + b + c;     //Sum of three variables
-
         System.out.println(result);
 
         b = 10;                     //Modify Value of variable b
 
         result = a + b + c;
-
         System.out.println(result);
     }
 }
