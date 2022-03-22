d3f33ad37a67a5cd163323de410332f70a474c07
SujanMore <sujanmore@gmail.com>
2022-03-22 18:32:10 +0530

    Check if a number is even or odd.

---
+++ Day1/EvenCheck.java
@@ -0,0 +1,11 @@
+public class EvenCheck {
+    public static void main(String[] args) {
+        int number = 70;
+
+        if(number % 2 == 0){                        //To check if number is even or not
+            System.out.println(number + " is an even number");
+        } else {
+            System.out.println((number + " is an odd number"));
+        }
+    }
+}
