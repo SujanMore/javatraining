29b773780cd4f96669b3469e568a475a088f78e0
SujanMore <sujanmore@gmail.com>
2022-03-22 19:12:41 +0530

    Created a method that prints four statements

---
+++ Day2/PrintFourStatement.java
@@ -0,0 +1,14 @@
+public class PrintFourStatement {
+
+    static void printStatements(){
+        System.out.println("I've created my first variable");
+        System.out.println("I've created my first method");
+        System.out.println("I've created my first loop");
+        System.out.println("I'm excited to learn java");
+    }
+    public static void main(String[] args) {
+
+        printStatements();                  //Call the method
+
+    }
+}
