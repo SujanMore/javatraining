7af2b0872f24f2eb88ad988c3c2bb9b8e99cb10d
SujanMore <sujanmore@gmail.com>
2022-03-23 17:45:10 +0530

    Create Class Books and its objects

--- Day3/Day3/src/Books/BooksRun.java
+++ Day3/Day3/src/Books/BooksRun.java
@@ -3,5 +3,16 @@ package Books;
 public class BooksRun {
     public static void main(String[] args) {
 
+        Books harryPotter = new Books("Harry Potter & the Philisopher's Stone", "J.K. Rowling", 300, 1997);
+
+        System.out.println(harryPotter);
+        System.out.println("");
+
+        harryPotter.setCopiesSold(15000);
+
+        System.out.println("Number of copies sold of book: " + harryPotter.getName() + " is " + harryPotter.getCopiesSold());
+        System.out.println("");
+        
+        System.out.println(harryPotter);
     }
 }
