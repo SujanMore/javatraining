d41c7d14d25e9b8371d66b856407393f5651ac8d
SujanMore <sujanmore@gmail.com>
2022-03-23 16:13:42 +0530

    Understanding the conepts of OOP

--- Day3/Day3/src/MotorBike/MotorBike.java
+++ Day3/Day3/src/MotorBike/MotorBike.java
@@ -17,11 +17,24 @@ public class MotorBike {
     public String getBikeName(){
         return this.bikeName;
     }
+
     public int getSpeed() {
         return this.speed;
     }
 
     public void setSpeed(int speed) {
         this.speed = speed;
+    }
+
+    public void increaseSpeed(int speed){           //Practising the concept of Encapsulation
+        setSpeed(this.speed + speed);
+    }
+
+    @Override                                       //Not explained in the course yet. Just trying it out.
+    public String toString() {
+        return "MotorBike{" +
+                "bikeName='" + bikeName + '\'' +
+                ", speed=" + speed +
+                '}';
     }
 }
--- Day3/Day3/src/MotorBike/MotorBikeRun.java
+++ Day3/Day3/src/MotorBike/MotorBikeRun.java
@@ -4,7 +4,15 @@ public class MotorBikeRun {
     public static void main(String[] args) {
 
         MotorBike suzuki = new MotorBike("Hayabusa", 40);
+        MotorBike bajaj = new MotorBike("Pulsar", 30);
 
         System.out.println("Speed of " + suzuki.getBikeName() + " is " + suzuki.getSpeed());
+
+        suzuki.setSpeed(100);
+        bajaj.increaseSpeed(50);
+
+        System.out.println(suzuki);
+
+        System.out.println(bajaj);
     }
 }
