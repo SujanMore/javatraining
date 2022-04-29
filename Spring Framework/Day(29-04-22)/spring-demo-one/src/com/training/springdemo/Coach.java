2393962dd54c2c4a28ee490c15dedff5ef65d282
SujanMore <sujanmore@gmail.com>
2022-04-29 16:00:27 +0530

    Exercise to learn dependency injection

---
+++ Spring Framework/Day(29-04-22)/spring-demo-one/src/com/training/springdemo/Coach.java
@@ -0,0 +1,7 @@
+package com.training.springdemo;
+
+public interface Coach {
+	
+	public String getDailyWorkout();
+
+}
---
+++ Spring Framework/Day(29-04-22)/spring-demo-one/src/com/training/springdemo/FootballCoach.java
@@ -0,0 +1,11 @@
+package com.training.springdemo;
+
+public class FootballCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "Ankle jump for 15 minutes";
+	}
+
+}
